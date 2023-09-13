59ff625fb2f36f6e2aecf0f6f33b1b548a2739d7
shrutanten <shrutanten.work@gmail.com>
2023-09-13 11:46:36 +0530

    Update reducer

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
+const redux = require("redux");
 const { createStore } = require("redux");
+const combineReducer = redux.combineReducers;
 // Single Action
 // npm install redux
 const BUY_CAKE = "BUY_CAKE";
 const BUY_MOBILE = "BUY_MOBILE";
-
-//Action creation
+//Action creatin
 function buyCake() {
 	return {
-		type: BUY_CAKE,
+		type: "BUY_CAKE",
 		message: "Cake Action......."
 	};
 }
 function buyMobile() {
 	return {
-		type: BUY_MOBILE,
+		type: "BUY_MOBILE",
 		message: "Cake Action......."
 	};
 }
-
 // initialState of the Object
 const initialCakeState = {
-	numberOfCakes: 10,
+	numberOfCakes: 10
+};
+initialMobileState = {
 	numberOfMobiles: 20
 };
-
-//Reducer
-const reducer = (state = initialCakeState, action) => {
+//Reducer1
+const cakeReducer = (state = initialCakeState, action) => {
 	switch (action.type) {
 		case BUY_CAKE:
 			return {
 				...state,
 				numberOfCakes: state.numberOfCakes - 1
 			};
+		default:
+			return state;
+	}
+};
+//Reducer2
+const mobileReducer = (state = initialMobileState, action) => {
+	switch (action.type) {
 		case BUY_MOBILE:
 			return {
 				...state,
@@ -41,9 +49,12 @@ const reducer = (state = initialCakeState, action) => {
 			return state;
 	}
 };
-
 //Store
-const store = createStore(reducer);
+const rootReducer = combineReducer({
+	cake: cakeReducer,
+	mobile: mobileReducer
+});
+const store = createStore(rootReducer);
 console.log("Initial State", store.getState());
 store.subscribe(() => console.log("Update state", store.getState()));
 store.dispatch(buyCake());
